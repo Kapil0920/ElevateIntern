29bf5ead73ba053fd0373b75b918151c25083971
Kapil0920 <k.kumarkapil08@gmail.com>
2025-06-23 18:02:03 +0530

    update

--- src/com/elevate/Calculator_Day1.java
+++ src/com/elevate/Calculator_Day1.java
@@ -2,78 +2,144 @@ package com.elevate;
 
 import java.util.Scanner;
 
+// Calculator_Day1: A simple calculator for basic arithmetic operations using user input.
 public class Calculator_Day1 {
+	
+	// Static Scanner to take input from the user
 	static Scanner sc = new Scanner(System.in);
-	public int addition(int count ) {
-		
-		int sum =0;
-		for(int i =1;i<=count;i++) {
-		System.out.println("Enter the number: "+i);
-		int num = sc.nextInt();
-		sum +=num;
+
+	
+//	===================================================
+	
+	
+	// Method for performing addition operation on 'count' numbers
+	public int addition(int count) {
+		int sum = 0;
+		for (int i = 1; i <= count; i++) {
+			System.out.println("Enter the number: " + i);
+			int num = sc.nextInt();
+			sum += num; // Add the input number to the running total
 		}
-		
 		return sum;
 	}
-	public int subtract(int count, int num1) {
-		 for(int i = 2; i<=count; i++) {
-			 
-			 System.out.print("Enter the number: "+i+" -");
-			 int num = sc.nextInt();
-			 
-			 num1 =num1-num;
-			 
-		 }
-		 return num1;
-		
+
 	
+	
+//	===================================================	
+	
+	
+	// Method for performing subtraction on 'count' numbers starting from 'num1'
+	public int subtract(int count, int num1) {
+		for (int i = 2; i <= count; i++) {
+			System.out.print("Enter the number: " + i + " - ");
+			int num = sc.nextInt();
+			num1 = num1 - num; // Subtract each subsequent number from the first
+		}
+		return num1;
 	}
 
+	
+	
+//	===================================================	
+	
+	// Method for performing multiplication on 'count' numbers
+	public int multiply(int count) {
+		int multiply = 1;
+		for (int i = 1; i <= count; i++) {
+			System.out.println("Enter the number: " + i);
+			int num = sc.nextInt();
+			multiply *= num; // Multiply each number to the result
+		}
+		return multiply;
+	}
+	
+	
+	
+//	===================================================
+
+	// Method to perform division operation
+	public int divide(int dividend, int divisor) {
+		System.out.println("Enter the dividend");
+		dividend = sc.nextInt(); // Get the dividend from user
+		System.out.println("Enter the divisor");
+		divisor = sc.nextInt(); // Get the divisor from user
+
+		return dividend / divisor; // Return the quotient
+	}
+//	===================================================
+	
+	
+	
+	
+	// Main method to drive the calculator application
 	public static void main(String[] args) {
-		Calculator_Day1 cal = new Calculator_Day1();
-		// TODO Auto-generated method stub
-		System.out.println("Hello Day");
-		
-		
-		
-		
-		
+		Calculator_Day1 cal = new Calculator_Day1(); // Create Calculator object
+		String forNextOperation;
+
 		do {
-			System.out.println("What do you want to do Add, Subtract, Multiply or Divide");
+			// Ask the user which operation they want to perform
+			System.out.println("What do you want to do Add, Subtract, Multiply or Divide, (type: add,sub,mul,div for perform operation)");
 			String answer = sc.next();
-			
-			
-			
-			if(answer.equalsIgnoreCase("Add")) {
-				System.out.println("How many number do you want to add");
-				int count = sc.nextInt();
 
+//			===================================================
+			
+			if (answer.equalsIgnoreCase("Add")) {
+				// Addition operation
+				System.out.println("How many numbers do you want to add");
+				int count = sc.nextInt();
 				int add = cal.addition(count);
-				System.out.println("Addition of number is: "+add);
-				
-			}
+				System.out.println("Sum is: " + add);
+			} 
 			
-			else if(answer.equalsIgnoreCase("subtract")) {
-				System.out.println("How many number do you want to subtract");
+			
+//			===================================================
+			
+			else if (answer.equalsIgnoreCase("sub")) {
+				// Subtraction operation
+				System.out.println("How many numbers do you want to subtract");
 				int count = sc.nextInt();
-				 System.out.println("Note: whatever number you will type that will automatically subtract from the \"FIRST\" number");
-
+				System.out.println("Note: whatever number you will type will be subtracted from the \"FIRST\" number");
 				System.out.println("Enter the number: 1");
-				int num1=sc.nextInt();
-
-				int subtract = cal.subtract(count,num1);
-				System.out.println("Subtraction of number is: "+subtract);
-				
+				int num1 = sc.nextInt();
+				int subtract = cal.subtract(count, num1);
+				System.out.println("Difference is: " + subtract);
+			} 
+			
+			
+//			===================================================
+			
+			else if (answer.equalsIgnoreCase("Mul")) {
+				// Multiplication operation
+				System.out.println("How many numbers do you want to multiply ");
+				int count = sc.nextInt();
+				System.out.println("\n");
+				int multiply = cal.multiply(count);
+				System.out.println("Product is: " + multiply);
+			} 
+			
+			
+//			===================================================
+			
+			else if (answer.equalsIgnoreCase("Div")) {
+				// Division operation
+				int quotient = cal.divide(1, 1); // Initial values are placeholders
+				System.out.println("Quotient is: " + quotient);
 			}
 			
-			else if(answer.equalsIgnoreCase("multiply")) {
-				
+			
+//			===================================================
+			else {
+				System.out.println("Invalid operation. Please choose add, sub, mul, or div.");
 			}
-			
-			
-			
-		}while(true);
 
+			
+			
+			// Ask if user wants to perform another operation
+			System.out.println("Do you want to do more operations? Type 'yes' or 'no'");
+			forNextOperation = sc.next();
+
+			
+			
+		} while (forNextOperation.equalsIgnoreCase("yes")); // Repeat if user says "yes"
 	}
-
 }
